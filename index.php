83d11fa0d10a28208b6192ca1f814454b8e57d83
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-07-20 16:25:11 +0200

    First template

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>
+			Homepage
+		</title>
+	</head>
+	
+	<body>
+		<header>
+			<nav>
+				<ul>
+					<li>
+						<h1>
+							Jepsenbrite
+						</h1>
+					</li>
+					<li>
+						Create event
+					</li>
+					<li>
+						Past events
+					</li>
+					<li>
+						Profile
+					</li>
+					<li>
+						Sign up
+					</li>
+					<li>
+						Log in
+					</li>
+				</ul>
+			</nav>
+		</header>
+
+
+		<main>
+			<h2>
+				Upcoming events
+			</h2>
+			
+			<template>
+				<div>
+					<figure>
+						<img src="" alt="">
+					</figure>
+
+					<h3>µ</h3>
+
+					<ul>
+						<li>
+						</li>
+						<li>
+						</li>
+					</ul>
+
+					<button>
+					</button>
+				</div>
+			</template>
+		</main>
+	</body>
+</html>
