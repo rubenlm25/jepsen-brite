2e58bdb4b5f62a46ec5152e17ce7be4cfdd99534
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-07-21 17:12:19 +0200

    Template putted aside

---
+++ template.php
@@ -0,0 +1,75 @@
+<!-- STRUCTURE DE BASE DE HOMEPAGE -->
+
+<!DOCTYPE html>
+
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>
+			Homepage
+		</title>
+	</head>
+	
+	<body>
+		<header>
+			<nav>
+				<ul>
+					<li>
+						<h1>
+							Jepsenbrite
+						</h1>
+					</li>
+					<li>
+						Create event
+					</li>
+					<li>
+						Past events
+					</li>
+					<li>
+						Profile
+					</li>
+					<li>
+						Sign up
+					</li>
+					<li>
+						Log in
+					</li>
+				</ul>
+			</nav>
+		</header>
+
+
+		<main>
+			<h2>
+				Upcoming events
+			</h2>
+			
+			
+			<!-- Template qui sera utilisé par PHP pour générer les events -->
+			<template>
+				<div>
+					<!-- Image de l'event -->
+					<figure>
+						<img src="" alt="">
+					</figure>
+
+					<!-- Titre de l'event -->
+					<h3></h3>
+
+					<!-- Date et heure de l'event -->
+					<ul>
+						<li>
+						</li>
+						<li>
+						</li>
+					</ul>
+
+					<!-- Bouton qui envoie sur la page event pour le voir en détail -->
+					<button>
+					</button>
+				</div>
+			</template>
+		</main>
+	</body>
+</html>
