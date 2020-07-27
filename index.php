5d7fc9e49c3714b708da0a3732f09eaa321d5acf
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-07-27 15:34:09 +0200

    Added link to the footer CSS

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" href="assets/scss/header_style.css">
+		<link rel="stylesheet" href="assets/scss/footer_style.css">
 		<link rel="stylesheet" href="assets/scss/index_main_part_style.css">
 		<link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@700&display=swap" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
