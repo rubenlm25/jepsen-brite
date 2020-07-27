f792e1af02c230a7b5e72d923721bcbe3489751c
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-07-27 15:33:35 +0200

    Added link to the main title

--- header.php
+++ header.php
@@ -3,23 +3,23 @@
 		<ul>
 			<li>
 				<h1>
-					Jepsenbrite
+					<a href="index.php" title="Go to the homepage" class="link_title">Jepsenbrite</a>
 				</h1>
 			</li>
 			<li>
-				<a href="" title="Create an event">Create Event</a>
+				<a href="" title="Create an event" class="links_menu">Create Event</a>
 			</li>
 			<li>
-				<a href="" title="See past events">Past Events</a>
+				<a href="past_events.php" title="See past events" class="links_menu">Past Events</a>
 			</li>
 			<li>
-				<a href="" title="Go to your profile">Profile</a>
+				<a href="" title="Go to your profile" class="links_menu">Profile</a>
 			</li>
 			<li>
-				<a href="" title="Register">Sign Up</a>
+				<a href="" title="Register" class="links_menu">Sign Up</a>
 			</li>
 			<li>
-				<a href="" title="Log in to your account">Log In</a>
+				<a href="" title="Log in to your account" class="links_menu">Log In</a>
 			</li>
 		</ul>
 	</nav>
