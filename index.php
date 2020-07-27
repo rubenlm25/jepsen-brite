c63829e0ff3e74c44ef513cd2b2804d0096755e1
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-07-27 09:30:38 +0200

    Homepage

--- index.php
+++ index.php
@@ -4,8 +4,10 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link rel="stylesheet" href="assets/scss/index_style.css">
+		<link rel="stylesheet" href="assets/scss/index_header_part_style.css">
+		<link rel="stylesheet" href="assets/scss/index_main_part_style.css">
 		<link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@700&display=swap" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
 		<title>
 			Homepage
 		</title>
@@ -21,19 +23,19 @@
 						</h1>
 					</li>
 					<li>
-						Create event
+						<a href="" title="Create an event">Create Event</a>
 					</li>
 					<li>
-						Past events
+						<a href="" title="See past events">Past Events</a>
 					</li>
 					<li>
-						Profile
+						<a href="" title="Go to your profile">Profile</a>
 					</li>
 					<li>
-						Sign up
+						<a href="" title="Register">Sign Up</a>
 					</li>
 					<li>
-						Log in
+						<a href="" title="Log in to your account">Log In</a>
 					</li>
 				</ul>
 			</nav>
@@ -42,12 +44,18 @@
 
 		<main>
 			<h2>
-				Upcoming events
+				Upcoming Events
 			</h2>
 		
-			<?php
-				include('assets/php/index_php_code.php');
-			?>
+			<div class='cards_container'>
+				<?php
+					include('assets/php/index_php_code.php');
+				?>
+			</div>
 		</main>
+
+
+		<footer>
+		</footer>
 	</body>
 </html>
