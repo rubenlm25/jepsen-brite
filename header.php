2d7b861a13b348efaad74cd3dedb23d4b84a4f2d
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-07-27 14:17:53 +0200

    Header section initialized

---
+++ header.php
@@ -0,0 +1,26 @@
+<header>
+	<nav>
+		<ul>
+			<li>
+				<h1>
+					Jepsenbrite
+				</h1>
+			</li>
+			<li>
+				<a href="" title="Create an event">Create Event</a>
+			</li>
+			<li>
+				<a href="" title="See past events">Past Events</a>
+			</li>
+			<li>
+				<a href="" title="Go to your profile">Profile</a>
+			</li>
+			<li>
+				<a href="" title="Register">Sign Up</a>
+			</li>
+			<li>
+				<a href="" title="Log in to your account">Log In</a>
+			</li>
+		</ul>
+	</nav>
+</header>
