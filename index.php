e2173d28ef6fc2fbbdf117d41640f893aa067dea
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-07-27 09:39:09 +0200

    Link modification

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link rel="stylesheet" href="assets/scss/index_header_part_style.css">
+		<link rel="stylesheet" href="assets/scss/header_style.css">
 		<link rel="stylesheet" href="assets/scss/index_main_part_style.css">
 		<link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@700&display=swap" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
