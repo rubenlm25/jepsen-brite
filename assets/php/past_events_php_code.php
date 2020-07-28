0ae30dd932b664b164a9a302f1a353b5a8f196ec
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-07-28 14:49:01 +0200

    Moved the connection to the database to a new file

--- assets/php/past_events_php_code.php
+++ assets/php/past_events_php_code.php
@@ -1,18 +1,6 @@
 <?php
 	// Connection to database
-	try
-	{
-		$database = new PDO
-		// Connection to database on Heroku
-		// ('mysql:host=us-cdbr-east-02.cleardb.com;dbname=heroku_f2e7be08f8f82c4;charset=utf8','b5a83bf957a94e','e7c157ba');
-
-		// Connection to database on localhost
-		('mysql:host=localhost;dbname=jepsen-brite;charset=utf8','root','');
-	}
-	catch (Exception $e)
-	{
-		die('Erreur : ' . $e -> getMessage());
-	}
+	require "connection_to_database.php";
 
 	// Request from database and sort by date and time from closest to farthest
 	$response = $database -> query(
