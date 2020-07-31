3a461880003652092bfef2e0086e884803565217
Ruben Lomo Maestu <64866074+rubenlm25@users.noreply.github.com>
2020-07-31 15:15:46 +0200

    restart

--- eventpage.php
+++ eventpage.php
@@ -1,15 +1,4 @@
-<?php
-if(isset($_POST['addcomment']))
-    $id =$_GET["id"];
-    $message=$_POST["comment"];
-    $author=$_SESSION["auth"]->username;
-    $date_time = date('Y-m-d H:i:s');
-    $bdd = new PDO('mysql:host=us-cdbr-east-02.cleardb.com;dbname=heroku_f2e7be08f8f82c4;charset=utf8','b5a83bf957a94e','e7c157ba');
-$request = $bdd -> prepare("INSERT INTO comment(author,message,id_event,date_time)VALUE (?,?,?,?)");
-$request ->execute(array($author,$message,$id,$date_time));
-header("location=eventpage.php?id=".$id);
 
-?>
 <html>
     <head>
         <meta charset="utf-8" />
@@ -43,21 +32,7 @@ header("location=eventpage.php?id=".$id);
                     if ($usersession === $test["author"] && $data["date_time"] > $actualdate){
                         echo "<a href='editevent.php?id=".$data["id"]."'>edit </a>";
                     }
-		if(isset($_SESSION["auth"])){
-                        echo "<form action='eventpage.php?id=".$id."' method='post'>
-                                <textarea name='comment' cols='50'>write a comment</textarea>
-                                <input type='submit' value='send' name='addcomment'>
-                              </form>";
-                    }
-                    $displaycomment=$bdd->prepare("SELECT * FROM comment WHERE id_event=? ORDER BY date_time");
-                    $displaycomment->execute(array($id));
-                    while($comment=$displaycomment->fetch()){
-                        echo "<div>
-                                    <p>".$comment["author"]."</p>
-                                    <p>".$comment["date_time"]."</p>
-                                    <p>.".$comment["message"]."</p>
-                              </div>";
-                    }
+		
                     
 
             ?>
