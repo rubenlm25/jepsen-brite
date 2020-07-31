88a184e14471a3887450c91d588513b6cfdeee54
Ruben Lomo Maestu <64866074+rubenlm25@users.noreply.github.com>
2020-07-31 14:59:44 +0200

    on retest

--- eventpage.php
+++ eventpage.php
@@ -1,3 +1,14 @@
+<?php
+    $id =$_GET["id"];
+    $message=$_POST["comment"];
+    $author=$_SESSION["auth"]->username;
+    $date_time = date('Y-m-d H:i:s');
+    $bdd = new PDO('mysql:host=us-cdbr-east-02.cleardb.com;dbname=heroku_f2e7be08f8f82c4;charset=utf8','b5a83bf957a94e','e7c157ba');
+$request = $bdd -> prepare("INSERT INTO comment(author,message,id_event,date_time)VALUE (?,?,?,?)");
+$request ->execute(array($author,$message,$id,$date_time));
+header("location=eventpage.php?id=".$id);
+
+?>
 <html>
     <head>
         <meta charset="utf-8" />
@@ -32,7 +43,7 @@
                         echo "<a href='editevent.php?id=".$data["id"]."'>edit </a>";
                     }
 		if(isset($_SESSION["auth"])){
-                        echo "<form action='commentpost.php?id=".$id."' method='post'>
+                        echo "<form action='eventpage.php?id=".$id."' method='post'>
                                 <textarea name='comment' cols='50'>write a comment</textarea>
                                 <input type='submit' value='send'>
                               </form>";
