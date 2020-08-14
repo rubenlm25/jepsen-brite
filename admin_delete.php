6473b007ca05cba4015389c45d08dbc8c18cf9ef
as <simana.annesophie@gmail.com>
2020-08-14 15:53:27 +0200

    push avant bascule master

--- admin_delete.php
+++ admin_delete.php
@@ -6,16 +6,19 @@ if (!$_SESSION['mdp']){
     header('Location:admin_login.php');
 
 }
+?>
+<?php
 if (isset($_GET['id']) AND !empty($_GET['id'])){
     $getid = $_GET['id'];
     $delete_member_info = $pdo->prepare('DELETE FROM users WHERE id = ?');
     $delete_member_info->execute(array($getid));
      $_SESSION['flash']['success'] = "utilisateur supprimé !";
-     header('Location:admin_events.php');
+     header('Location:admin_members.php');
 }else{
     $_SESSION['flash']['danger']= "utilisateur introuvable";
 }
-
+?>
+<?php
 if (isset($_GET['id']) AND !empty($_GET['id'])){
     $getid = $_GET['id'];
     $delete_events = $pdo->prepare('DELETE FROM event WHERE id = ?');
@@ -25,11 +28,13 @@ if (isset($_GET['id']) AND !empty($_GET['id'])){
 }else{
     $_SESSION['flash']['danger']= "événement  introuvable";
 }
+?>
 
+<?php
 if (isset($_GET['id']) AND !empty($_GET['id'])){
-    $getid = $_GET['id'];
+    $getid_com = $_GET['id'];
     $delete_com = $pdo->prepare('DELETE FROM comment WHERE id = ?');
-    $delete_com->execute(array($getid));
+    $delete_com->execute(array($getid_com));
     $_SESSION['flash']['success'] = "comment delete successfully !";
     header('Location:admin_comments.php');
 }else{
